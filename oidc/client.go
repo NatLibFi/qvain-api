ac0f5daaf37189ed0292295c454e3e0674487195
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-06-26 17:13:36 +0300

    Add proper OIDC state protection and improve logging

--- oidc/client.go
+++ oidc/client.go
@@ -3,14 +3,23 @@ package oidc
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+	"time"
+
+	"github.com/NatLibFi/qvain-api/randomkey"
 
 	gooidc "github.com/coreos/go-oidc"
-
 	"github.com/rs/zerolog"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
+)
+
+const (
+	// DefaultLoginTimeout is the age, in seconds, of the state cookie during OIDC login.
+	DefaultLoginTimeout = 600 // 10m
+
+	// DefaultCookiePath sets the URL path cookies from this package are valid for.
+	DefaultCookiePath = "/api/auth"
 )
 
 type OidcClient struct {
@@ -60,7 +69,26 @@ func NewOidcClient(id string, secret string, redirectUrl string, providerUrl str
 
 func (client *OidcClient) Auth() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("redirect:", client.oauthConfig.AuthCodeURL(client.state))
+		key, err := randomkey.Random16()
+		if err != nil {
+			client.logger.Error().Err(err).Msg("can't create state parameter")
+			http.Error(w, "can't create state parameter", http.StatusInternalServerError)
+			return
+		}
+		state := key.Base64()
+
+		http.SetCookie(w, &http.Cookie{
+			Name:  "state",
+			Value: state,
+			Path:  DefaultCookiePath,
+			// old browsers such as IE<=8 don't understand MaxAge; use Expires or leave it unset to make this a "session cookie"
+			Expires:  time.Now().Add(DefaultLoginTimeout * time.Second),
+			MaxAge:   DefaultLoginTimeout,
+			Secure:   true,
+			HttpOnly: true,
+		})
+		client.logger.Debug().Str("state", state).Msg("redirect to IdP")
+		//log.Println("redirect:", client.oauthConfig.AuthCodeURL(client.state))
 		http.Redirect(w, r, client.oauthConfig.AuthCodeURL(client.state), http.StatusFound)
 	}
 }
@@ -68,7 +96,70 @@ func (client *OidcClient) Auth() http.HandlerFunc {
 func (client *OidcClient) Callback() http.HandlerFunc {
 	ctx := context.Background()
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("q: %+v\n", r.URL.Query())
+		//log.Printf("q: %+v\n", r.URL.Query())
+
+		cookie, err := r.Cookie("state")
+		if err != nil {
+			client.logger.Debug().Msg("no state cookie")
+			http.Error(w, "login session expired", http.StatusBadRequest)
+			return
+		}
+
+		if r.URL.Query().Get("state") != cookie.Value {
+			client.logger.Debug().Str("param", r.URL.Query().Get("state")).Str("cookie", cookie.Value).Msg("state did not match")
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := client.oauthConfig.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			client.logger.Error().Err(err).Msg("token exchange failed")
+			http.Error(w, "failed to exchange code for token", http.StatusInternalServerError)
+			return
+		}
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			client.logger.Error().Msg("id_token missing from IdP response")
+			http.Error(w, "IdP did not sent an id token", http.StatusInternalServerError)
+			return
+		}
+		idToken, err := client.oidcVerifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			client.logger.Error().Err(err).Msg("id token does not verify")
+			http.Error(w, "id token verification failed", http.StatusInternalServerError)
+			return
+		}
+
+		oauth2Token.AccessToken = "*REDACTED*"
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+		}{oauth2Token, new(json.RawMessage)}
+
+		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+			client.logger.Error().Err(err).Msg("failed to extract claims")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		client.logger.Info().Str("sub", idToken.Subject).Msg("login")
+
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			client.logger.Error().Err(err).Msg("failed to marshal json response")
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		http.Redirect(w, r, "/token#"+rawIDToken, http.StatusFound)
+		w.Write(data)
+	}
+}
+
+func (client *OidcClient) DebugCallback() http.HandlerFunc {
+	ctx := context.Background()
+	return func(w http.ResponseWriter, r *http.Request) {
+		//log.Printf("q: %+v\n", r.URL.Query())
 		if r.URL.Query().Get("state") != client.state {
 			http.Error(w, "state did not match", http.StatusBadRequest)
 			return
---
+++ randomkey/randomkey.go
@@ -0,0 +1,73 @@
+// Package randomkey generates cryptographically secure keys of lengths 16, 32 or 64.
+package randomkey
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+type Key []byte
+
+func (k Key) Bytes() []byte {
+	return k
+}
+
+func (k Key) Hex() string {
+	return hex.EncodeToString(k)
+}
+
+func FromHex(s string) (*Key, error) {
+	key, err := hex.DecodeString(s)
+	return (*Key)(&key), err
+}
+
+func (k Key) Base64() string {
+	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(k)
+}
+
+func FromBase64(s string) (*Key, error) {
+	key, err := base64.URLEncoding.WithPadding(base64.NoPadding).DecodeString(s)
+	return (*Key)(&key), err
+}
+
+func (k Key) Len() int {
+	return len(k)
+}
+
+func (k Key) String() string {
+	return hex.EncodeToString(k)
+}
+
+func (k Key) GoString() string {
+	//return fmt.Sprintf("Key(0x%s)", k.Hex())
+	//return fmt.Sprintf("Key(0x %x)", k)
+	return "Key(" + k.Hex() + ")"
+}
+
+func genRandomBytes(n int) (*Key, error) {
+	var key Key = make([]byte, n)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return &Key{}, fmt.Errorf("error generating random key:", err)
+	}
+	return &key, nil
+}
+
+func Random8() (*Key, error) {
+	return genRandomBytes(8)
+}
+
+func Random16() (*Key, error) {
+	return genRandomBytes(16)
+}
+
+func Random32() (*Key, error) {
+	return genRandomBytes(32)
+}
+
+func Random64() (*Key, error) {
+	return genRandomBytes(64)
+}
